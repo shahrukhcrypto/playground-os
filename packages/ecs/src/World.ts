b2f8cc43dbf6ec5ec3e60511b951f4db834dc845
shahrukhcrypto <shahrukhcrypto1980@gmail.com>
2026-07-06 16:36:51 +0530

    feat: add entity lifecycle

--- packages/ecs/src/World.ts
+++ packages/ecs/src/World.ts
@@ -1,10 +1,16 @@
-export class World {}
 import { Entity } from "./Entity";
 
 export class World {
   private nextEntity = 0;
+  private entities = new Set<Entity>();
 
   createEntity(): Entity {
-    return this.nextEntity++;
+    const entity = this.nextEntity++;
+    this.entities.add(entity);
+    return entity;
+  }
+
+  destroyEntity(entity: Entity): void {
+    this.entities.delete(entity);
   }
 }
