75f3549048f3e5550cc2606d7157716cbd2f8cc3
shahrukhcrypto <shahrukhcrypto1980@gmail.com>
2026-07-06 16:37:30 +0530

    feat: implement query storage

--- packages/ecs/src/Query.ts
+++ packages/ecs/src/Query.ts
@@ -1,1 +1,21 @@
-export class Query {}
+import { Entity } from "./Entity";
+
+export class Query {
+  private entities = new Set<Entity>();
+
+  add(entity: Entity): void {
+    this.entities.add(entity);
+  }
+
+  remove(entity: Entity): void {
+    this.entities.delete(entity);
+  }
+
+  has(entity: Entity): boolean {
+    return this.entities.has(entity);
+  }
+
+  getAll(): Entity[] {
+    return [...this.entities];
+  }
+}
