a60bfadb50945804b40ef33a111c253af5eb5a5c
shahrukhcrypto <shahrukhcrypto1980@gmail.com>
2026-07-06 16:45:04 +0530

    feat: add system enable state

--- packages/ecs/src/System.ts
+++ packages/ecs/src/System.ts
@@ -1,7 +1,8 @@
 import { World } from "./World";
 
 export abstract class System {
-  protected world: World;
+  protected readonly world: World;
+  public enabled = true;
 
   constructor(world: World) {
     this.world = world;
